46aa0cf0b490ad976ba25455a3ca0072f496338b
Paula Jach <paulajach92@gmail.com>
2020-03-08 21:21:17 +0100

    created weather container, which is showed, when user will search city

---
+++ calendar.js
@@ -0,0 +1,21 @@
+const actualDate = document.getElementById("date");
+const options = {weekday: "long", month: "short", day: "numeric"};
+actualDate.innerHTML = new Date().toLocaleDateString("en-US", options);
+
+function showTime() {
+    const today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+    let seconds = today.getSeconds();
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    const time = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("clock").innerText = time;
+    document.getElementById("clock").textContent = time;
+
+    setTimeout(showTime, 1000);
+}
+showTime();
---
+++ index.js
@@ -0,0 +1,62 @@
+const searchWeatherButton = document.querySelector('#searchWeatcher');
+
+searchWeatherButton.addEventListener('click', (e) => {
+    const searchCity = document.querySelector('#city');
+    const cityName = searchCity.value;
+    e.preventDefault();
+    const searchWeather = () => {
+        let weatherBox = document.querySelector('#weather');
+        let countryName = document.querySelector('#country');
+        let weatherDetails = document.querySelector('#weatherDescription');
+        let temperature = document.querySelector('#weatherTemperature');
+        const apiKey = '7060ee9b041b9b36a67d9bc06873b71b';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+    
+        if (cityName.length === 0) {
+            return alert('Please enter a City Name!');
+        };
+    
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                "Accept": "application.json"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if(data.length !== 0) {
+                console.log(data);
+                searchedCity = cityName;
+                countryName = data.sys.country;
+                weatherDetails = data.weather[0].description.toUpperCase();
+                temperature = data.main.temp;
+                updateWeather(weatherBox);
+            }
+            else {
+                alert('Something went wrong');
+            } 
+        });
+        
+        const updateWeather = () => {
+            weatherBox.classList.remove('hide');
+            weatherCity.innerHTML = cityName;
+            country.innerHTML = `Country: ${countryName}`;
+            weatherDescription.innerHTML = `Current weather: ${weatherDetails}`;
+            weatherTemperature.innerHTML = `Temperature: ${temperature} \xB0C`;
+            
+            const convertBtn = document.querySelector('#convertTemp');
+
+            convertBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+                const convertTemperature = () => {
+                    let convertTempToFahrenheit = (temperature * 9 / 5 + 32).toFixed(2);
+                    let showConvertedTemp = document.querySelector('#converted');
+                    showConvertedTemp.innerHTML = `Temperature: ${convertTempToFahrenheit} \xB0F`;  
+                }
+                convertTemperature();   
+            });  
+        }; 
+    };
+    searchWeather();
+});
+
